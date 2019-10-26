3b339436edcf1f5230618f80fb673b581531a049
Hieu Tran <minhhieutran11197@gmail.com>
2019-10-26 15:14:26 +0700

    fix

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -1,6 +1,10 @@
+pm2 delete TICKET_BACKEND
+
 pm2 status
 
 cd ticket-sales-back
+
+npm rebuild bcrypt --update-binary
 
 echo "--------------Start app--------------------"
 pm2 start --name TICKET_BACKEND npm -- run start:prod
